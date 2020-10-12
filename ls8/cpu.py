760a20f0c48ceffb96a51edb98499550b7ff3025
Drew Ross <mail.drew.ross@gmail.com>
2020-10-12 15:18:58 -0700

    Add opcodes. Add logic for LDI, PRN, HLT. Print 8 with hardcoded program.

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -2,6 +2,7 @@
 
 import sys
 
+
 class CPU:
     """Main CPU class."""
 
@@ -10,8 +11,43 @@ class CPU:
         self.ram = [0] * 256
         self.reg = [0] * 8
         self.pc = 0
-        self.ir = 0
         self.fl = 0
+        self.opcode = {
+            "ADD": 0b10100000,
+            "AND": 0b10101000,
+            "CALL": 0b01010000,
+            "CMP": 0b10100111,
+            "DEC": 0b01100110,
+            "DIV": 0b10100011,
+            "HLT": 0b00000001,
+            "INC": 0b01100101,
+            "INT": 0b01010010,
+            "IRET": 0b00010011,
+            "JEQ": 0b01010101,
+            "JGE": 0b01011010,
+            "JGT": 0b01010111,
+            "JLE": 0b01011001,
+            "JLT": 0b01011000,
+            "JMP": 0b01010100,
+            "JNE": 0b01010110,
+            "LD": 0b10000011,
+            "LDI": 0b10000010,
+            "MOD": 0b10100100,
+            "MUL": 0b10100010,
+            "NOP": 0b00000000,
+            "NOT": 0b01101001,
+            "OR": 0b10101010,
+            "POP": 0b01000110,
+            "PRA": 0b01001000,
+            "PRN": 0b01000111,
+            "PUSH": 0b01000101,
+            "RET": 0b00010001,
+            "SHL": 0b10101100,
+            "SHR": 0b10101101,
+            "ST": 0b10000100,
+            "SUB": 0b10100001,
+            "XOR": 0b10101011,
+        }
 
     def load(self):
         """Load a program into memory."""
@@ -22,30 +58,30 @@ class CPU:
 
         program = [
             # From print8.ls8
-            0b10000010, # LDI R0,8
+            0b10000010,  # LDI R0,8
             0b00000000,
             0b00001000,
-            0b01000111, # PRN R0
+            0b01000111,  # PRN R0
             0b00000000,
-            0b00000001, # HLT
+            0b00000001,  # HLT
         ]
 
         for instruction in program:
             self.ram[address] = instruction
             address += 1
 
-    def ram_read(self, address):
-        return self.ram[address]
+    def ram_read(self, mar):
+        return self.ram[mar]
 
-    def ram_write(self, data, address):
-        self.ram[address] = data
+    def ram_write(self, mdr, mar):
+        self.ram[mar] = mdr
 
     def alu(self, op, reg_a, reg_b):
         """ALU operations."""
 
         if op == "ADD":
             self.reg[reg_a] += self.reg[reg_b]
-        #elif op == "SUB": etc
+        # elif op == "SUB": etc
         else:
             raise Exception("Unsupported ALU operation")
 
@@ -55,20 +91,44 @@ class CPU:
         from run() if you need help debugging.
         """
 
-        print(f"TRACE: %02X | %02X %02X %02X |" % (
-            self.pc,
-            #self.fl,
-            #self.ie,
-            self.ram_read(self.pc),
-            self.ram_read(self.pc + 1),
-            self.ram_read(self.pc + 2)
-        ), end='')
+        print(
+            f"TRACE: %02X | %02X %02X %02X |"
+            % (
+                self.pc,
+                # self.fl,
+                # self.ie,
+                self.ram_read(self.pc),
+                self.ram_read(self.pc + 1),
+                self.ram_read(self.pc + 2),
+            ),
+            end="",
+        )
 
         for i in range(8):
-            print(" %02X" % self.reg[i], end='')
+            print(" %02X" % self.reg[i], end="")
 
         print()
 
     def run(self):
         """Run the CPU."""
-        pass
+        halt = False
+        
+        while not halt:
+            ir = self.ram_read(self.pc)
+
+            if ir == self.opcode["LDI"]:
+                reg_a = self.ram_read(self.pc + 1)
+                mdr = self.ram_read(self.pc + 2)
+                self.reg[reg_a] = mdr
+                self.pc += 3
+
+            elif ir == self.opcode["PRN"]:
+                reg_a = self.ram_read(self.pc + 1)
+                print(self.reg[reg_a])
+                self.pc += 2
+
+            elif ir == self.opcode["HLT"]:
+                halt = True
+                
+            else:
+                print("Unknown opcode. Ending program.")
